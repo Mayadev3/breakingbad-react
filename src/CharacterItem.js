53e5db3b233f9afa5d50fef2745cd1891fa06883
Mayadev3 <107764065+Mayadev3@users.noreply.github.com>
2022-10-06 23:10:16 +0200

    retrieved specific info for each character from the api

--- src/CharacterItem.js
+++ src/CharacterItem.js
@@ -3,7 +3,31 @@ import "./CharacterItem.css";
 export default function CharacterItem(props) {
   return (
     <div className="CharacterItem">
-      <img src={props.item.img} alt="character" />
+      {/*<img src={props.item.img} alt="character" />*/}
+      <div className="card">
+        <div className="card-inner">
+          <div className="card-front">
+            <img src={props.item.img} alt="" />
+          </div>
+          <div className="card-back">
+            <h1>{props.item.name}</h1>
+            <ul>
+              <li>
+                <strong>Actor Name:</strong> {props.item.portrayed}
+              </li>
+              <li>
+                <strong>Nickname:</strong> {props.item.nickname}
+              </li>
+              <li>
+                <strong>Birthday:</strong> {props.item.birthday}
+              </li>
+              <li>
+                <strong>Status:</strong> {props.item.status}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
